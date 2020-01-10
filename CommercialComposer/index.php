56c486f9e772296bcb0eff6482b5f61942c2bf9f
chri002 <56335168+chri002@users.noreply.github.com>
2020-01-10 19:21:08 +0100

    Add files via upload

--- CommercialComposer/index.php
+++ CommercialComposer/index.php
@@ -13,59 +13,85 @@
 	<script src='lib/BufferGeometryUtils.js'></script>
 	<script type="text/x-glsl"  id="fragment">
 		
+		
+		//variabili provenienti dal Vertex 
 		varying vec3 vPosition;
 		varying vec3 wPosition;
 					
 		varying vec3 vNormal;
 		varying vec2 vUv;
-		uniform float MAX_LIGHT;
-		uniform vec3 lightPosition[5];
-		uniform vec3 Clight;
-		uniform vec3 Color;
-		uniform float FattBlend;
-		uniform float FattEmiss;
-		uniform sampler2D specularMap;
-		uniform sampler2D diffuseMap;
-		uniform sampler2D roughnessMap;
-		uniform sampler2D normalMap;
-		uniform sampler2D emissionMap;
-		uniform samplerCube envMap;
-		uniform vec2 normalScale;
-		uniform vec2 textureRepeat;
-		uniform int type;
+		
+		
+		
+		uniform float MAX_LIGHT;				//Numero di luci
+		uniform vec3 lightPosition[5];			//Array di luci
+		uniform vec3 Clight;					//Colore di tutte le luci
+		uniform vec3 Color;						//Colore moltiplicativo della BRDF
+		uniform float FattBlend;				//valore di scelta Glossy<-->Diffuse
+		uniform float FattEmiss;				//valore di scelta BRDF<-->Emission
+		uniform sampler2D specularMap;			//Immmagine Specular
+		uniform sampler2D diffuseMap;			//Immagine Diffuse
+		uniform sampler2D roughnessMap;			//Immagine Rugosità
+		uniform sampler2D normalMap;			//Immagine Normale
+		uniform sampler2D emissionMap;			//Immagine Emission
+		uniform samplerCube envMap;				//Immagine Cubica d'Ambiente
+		uniform vec2 normalScale;				//valore moltiplicativo sulla normale
+		uniform vec2 textureRepeat;				//numero duplicazioni (x,y) delle immagini
+		uniform int type;						//Tipo miscelamento (0:Diffuse, 1:tessuto)
 		
 		
 		const float PI = 3.14159;
 		
 		vec3 cdiff;
 		vec3 cspec;
-		vec3 cAO;
+		vec3 cEm;
 		vec3 emiss;
 		float roughness;
 		
+		//Schlick Frensel con con riflessione sphericalGaussiana 
+		//specular	: mappa speculare
+		//VdotH		: dot(Camera dirView e half Vector)
 		vec3 SchlickFresnelCustom(vec3 specular, float VdotH)
 		{
 			float sphericalGaussian = pow(2.0, (-5.55473 * VdotH - 6.98316) * VdotH);
 			return specular + (vec3(1.0, 1.0, 1.0) - specular) * sphericalGaussian;
 		}
-
+			
+		//Valore di Frensel
+		//specular	: mappa speculare
+		//VdotH		: dot(Camera dirView e half Vector)
 		vec3 Fresnel(vec3 specular, float VdotH)
 		{
 			return SchlickFresnelCustom(specular, VdotH);
 		}
 		
+		/*
+			
+			D_Charlie per la distribuzione del colore per simulare i tessuti/velvet
+			roughness	: mappa ruvidezza
+			NoH			: dot(Normale e half Vector)
+			
+			Estevez and Kulla 2017, "Production Friendly Microfacet Sheen BRDF"
+		*/
 		float D_Charlie(float roughness, float NoH) {
-			// Estevez and Kulla 2017, "Production Friendly Microfacet Sheen BRDF"
 			float invAlpha  = 1.0 / max(roughness, 0.0000001);
 			float cos2h = NoH * NoH;
-			float sin2h = max(1.0 - cos2h, 0.007874); // 2^(-14/2), so sin2h^2 > 0 in fp16
+			float sin2h = max(1.0 - cos2h, 0.007874); // 2^(-14/2): sin2h^2 > 0
 			return (4.0 + invAlpha) * pow(sin2h, invAlpha * 0.5) / (2.0 * PI);
 		}
 		
+		//Valore di distribuzione del colore per simulare i tessuti/velvet
+		//roughness	: mappa ruvidezza
+		//NoH		: dot(Normale e half Vector)
 		float distributionCloth(float roughness, float NoH) {
 			return D_Charlie(roughness, NoH);
 		}
 		
+		
+		//Glossy function SmithGGXSchlick 
+		//NdotL		: dot(Normale e lightPos)
+		//NdotV		: dot(Normale e Camera dirView)
+		//roughness	: mappa ruvidezza
 		float SmithGGXSchlickVisibility(float NdotL, float NdotV, float roughness)
 		{
 			float rough2 = roughness * roughness;
@@ -75,28 +101,30 @@
 			return 0.5 / (lambdaV + lambdaL);
 		}
 		
+		//funzione di Diffuse:lambertian
+		//diffuseColor : colore
 		vec3 LambertianDiffuse(vec3 diffuseColor)
 		{
 			return diffuseColor * (1.0 / PI);
 		}
 		
-		// Custom Lambertian Diffuse
+		// funzione Diffuse: Lambertian modificata con valori di ruvidezza
 		// diffuseColor : il colore
 		// roughness    : roughness
 		// NdotV        : dot(Normale e Camera dirView)
-		// NdotL        = dot(Normale e posLuce)
-		// VdotH        = dot(Camera dirView e half Vector)
+		// NdotL        : dot(Normale e posLuce)
+		// VdotH        : dot(Camera dirView e half Vector)
 		vec3 CustomLambertianDiffuse(vec3 diffuseColor, float NdotV, float roughness)
 		{
 			return diffuseColor * (1.0 / PI) * pow(NdotV, 0.5 + 0.3 * roughness);
 		}
 		
-		// Burley Diffuse
+		// funzione Diffuse: Burley(Disney) Diffuse
 		// diffuseColor : il colore
 		// roughness    : roughness
 		// NdotV        : dot(Normale e Camera dirView)
-		// NdotL        = dot(Normale e posLuce)
-		// VdotH        = dot(Camera dirView e half Vector)
+		// NdotL        : dot(Normale e posLuce)
+		// VdotH        : dot(Camera dirView e half Vector)
 		vec3 BurleyDiffuse(vec3 diffuseColor, float roughness, float NdotV, float NdotL, float VdotH)
 		{
 			float energyBias = mix(roughness, 0.0, 0.5);
@@ -111,6 +139,9 @@
 		
 		#extension GL_OES_standard_derivatives : enable
 		
+		//funzione per normale delle mcrofacce
+		//eye_pos		: Camera Position)
+		//surf_norm		: Normale
 		vec3 perturbNormal2Arb( vec3 eye_pos, vec3 surf_norm ) {
 			vec3 q0 = dFdx( eye_pos.xyz );
 			vec3 q1 = dFdy( eye_pos.xyz );
@@ -125,11 +156,14 @@
 			return normalize( tsn * mapN );
 		}
 		
+		//inversione di direzione
+		//dir		: direzione
+		//matrix	: matrice con cui invertire
 		vec3 inverseTransformDirection( in vec3 dir, in mat4 matrix ) {
 			return normalize( ( vec4( dir, 0.0 ) * matrix ).xyz );
 		}
 		
-		
+		//colore di riflessione della skybox
 		vec3 environment(){
 			vec3 n = perturbNormal2Arb( vPosition, normalize( vNormal ));  // interpolation destroys normalization, so we have to normalize
 			vec3 worldN = inverseTransformDirection( n, viewMatrix );
@@ -139,7 +173,8 @@
 			
 		}
 		
-				
+		//BRDF modificata data la posizione della luce
+		//lightPos	: posizione luce
 		vec3 BRDFNorm(vec3 lightPos){
 			
 			vec4 lPosition = viewMatrix * vec4( lightPos, 1.0 );
@@ -155,19 +190,21 @@
 			float vDoth = min(max(dot( v, h ),0.0000001),1.0);
 			float nDotv = min(max(dot( n, v ),0.0000001),1.0);
 			
-			vec3 reFlect = environment();
+			vec3 reFlect = environment();																							//colore di riflessione del skybox
 			
 			vec3 fresnel = Fresnel(cspec, vDoth);
-			vec3 BRDF = ((vec3(FattBlend)*fresnel)*((reFlect*SmithGGXSchlickVisibility(nDotl,nDotv, roughness))));
-			vec3 Burley = (BurleyDiffuse(cdiff,roughness,nDotv,nDotl, nDoth));
+			vec3 BRDF = ((vec3(FattBlend)*fresnel)*((reFlect*SmithGGXSchlickVisibility(nDotl,nDotv, roughness))));					//Glossy part
+			BRDF *=(min(max(0.0, 1.0/length(vPosition)), 1.0));
+			vec3 Burley = (BurleyDiffuse(cdiff,roughness,nDotv,nDotl, nDoth));														//Burley color
 			//vec3 Lambertian = LambertianDiffuse(cdiff);
 			//vec3 LambertianD = CustomLambertianDiffuse(cdiff,nDotv,roughness);
-			float cloth = distributionCloth((roughness), (nDoth));
-			cloth = max(cloth, 0.025);
 			if(type==1){
-				BRDF = BRDF*(1.0-min(max(0.0, length(vPosition)), 1.0))+((vec3(1.0-FattBlend))*Burley * (cloth))+cloth*0.05;
+				float cloth = distributionCloth((roughness), sqrt(nDotv)*0.8+nDoth*0.2);											//cloth distribution
+				cloth = max(cloth, 0.0005);																							//rimuove colore troppo scuro
+			
+				BRDF += ((vec3(1.0-FattBlend))*Burley * (cloth))*(min(max(0.0, 1.0/length(vPosition)), 1.0));						//Diffuse part con tessuto							
 			}else{
-				BRDF += 1.0/length(lPosition)*Burley;
+				BRDF += 1.0/length(lPosition)*Burley;																				//Diffuse part
 			}
 			
 			return nDotl*BRDF/2.0;
@@ -187,15 +224,18 @@
 			cspec = pow( cspec, vec3(2.2));
 			roughness = texture2D( roughnessMap, vUv*textureRepeat).r; // no need to linearize roughness map
 
+			//BRDF una per ogni luce
 			vec3 BRDFTot = vec3(0.0);
 			for(int i=0; i<5; i++){
 					BRDFTot += BRDFNorm(lightPosition[i]);
 			}
 			BRDFTot =  BRDFTot/MAX_LIGHT ;
 			
-			cAO = texture2D(emissionMap, vUv*textureRepeat).rgb;
-			cAO = pow( cAO, vec3(3.5));
-			vec3 outRadiance = (max((1.0-FattEmiss),0.0)*(PI* Clight * BRDFTot)*(1.0-cAO)+ (FattEmiss*cAO*cdiff*PI))*Color;
+			//coeff emission
+			cEm = texture2D(emissionMap, vUv*textureRepeat).rgb;
+			cEm = pow( cEm, vec3(3.5));
+			
+			vec3 outRadiance = (max((1.0-FattEmiss),0.0)*(PI* Clight * BRDFTot)*(1.0-cEm)+ (FattEmiss*cEm*cdiff*PI))*Color;			//Mix tra BRDF e emission
 			// gamma encode the final value
 			gl_FragColor = vec4(pow( outRadiance, vec3(1.0/2.2)), 1.0);
 		}				
@@ -314,7 +354,7 @@
 			var textureIndex1 = 2, textureIndex2 = 1;
 			var TextureCopertura = new Array("Copertura_Tessuto1","Copertura_Tessuto2","Copertura_Tessuto3");
 			var TexturePlastica = new Array("Plastic","Wood","Iron");
-			var MaterialRoughness = new Array(new Array(0.05, 0.05, 0.1), new Array(0.025, 0.15, 0.1), new Array(0.05, 1.25, 0.1));
+			var MaterialRoughness = new Array(new Array(0.05, 0.005, 0.1), new Array(0.025, 0.15, 0.1), new Array(0.05, 1.25, 0.1));
 			
 			var textureParametersCopertura = {
 				material: TextureCopertura[textureIndex1],
@@ -524,10 +564,10 @@
 						for(var i=0; i<5; i++){
 							OBJModel.material[i] = (new THREE.ShaderMaterial({ uniforms : {
 								type : {type: "i", value: MaterialValue(i).type},
-								MAX_LIGHT : {type: "f", value: 5.0},
+								MAX_LIGHT : {type: "f", value: 4.0},
 								FattBlend : {type: "f", value: MaterialValue(i).roughness},
 								FattEmiss : {type: "f", value: MaterialValue(i).emission},
-								lightPosition  : { type:"v3v", value: [ new THREE.Vector3(7.5,2.5,0.0),new THREE.Vector3(-3.75,2.5,6.499),new THREE.Vector3(0.0,-500.0,0.0),new THREE.Vector3(0.0,7.5,0.0),new THREE.Vector3(-3.75,2.5,-6.499)]},
+								lightPosition  : { type:"v3v", value: [ new THREE.Vector3(10.0,3.0,0.0),new THREE.Vector3(-5.0,3.0,8.660),new THREE.Vector3(0.0,11.4017,0.0),new THREE.Vector3(0.0,-1000.0,0.0),new THREE.Vector3(-5.0,2.5,-8.660)]},
 								Clight    : { type:"v3", value: new THREE.Vector3(1.0,0.95,0.8)},
 								Color    : { type:"v3", value: MaterialValue(i).color},
 								normalMap: {type: "t", value: scegli(i,"Normal")},
